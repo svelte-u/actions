3790fdbf268817253f2948a0cd4985ffe699d9fb
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2023-02-27 10:47:05 +0300

    test(dropzone): add simple test

---
+++ src/dropzone/index.test.ts
@@ -0,0 +1,9 @@
+import { describe, expect, it } from "vitest"
+
+import { dropzone } from "."
+
+describe("dropzone", () => {
+	it("should be defined", () => {
+		expect(dropzone).toBeDefined()
+	})
+})
